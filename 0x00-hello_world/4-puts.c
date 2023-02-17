c612dc183396c21a149633c0665d4c528cd8060c
DanielReborn <danielreborn2019@gmail.com>
2023-02-17 03:34:08 -0800

    4-puts modified 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-  * main - Prints a text
+  * main -Entry point
   *
   * Return: Always 0 (Success)
   */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
 }
