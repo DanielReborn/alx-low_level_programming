2f6eea8dc0a75d1c4250414f93a3e8f6044b90c2
DanielReborn <danielreborn2019@gmail.com>
2023-05-05 08:46:32 +0100

    header file

---
+++ 0x07-pointers_arrays_strings/pointers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	int january = 23;
+	printf("value:%d\n", january);
+	printf("address:%p\n", &january);
+
+}
---
+++ 0x07-pointers_arrays_strings/three.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+	int grades[] = {30, 78, 90, 54};
+	printf("%d\n", grades[0]);
+}
